52f48a673ecdd6cb696f8decd63969f29a58e608
Michael Hadley <michael.hadley@liferay.com>
2017-02-10 17:30:10 -0800

    Give option to disable modal blur

--- spec/ModalHandler-spec.js
+++ spec/ModalHandler-spec.js
@@ -20,6 +20,8 @@ describe('ModalHandler', () => {
   })
 
   it('should add attribute with value of true when a modal is open', () => {
+    atom.config.set('outlander-ui.showBlur', true)
+
     const workspaceElement = atom.views.getView(atom.workspace)
     expect(workspaceElement.classList.contains(CLASSNAME)).toBe(false)
 
@@ -27,6 +29,17 @@ describe('ModalHandler', () => {
 
     expect(workspaceElement.classList.contains(CLASSNAME)).toBe(true)
     testModal.destroy()
+    expect(workspaceElement.classList.contains(CLASSNAME)).toBe(false)
+  })
+
+  it('should not add attribute if setting is disabled', () => {
+    atom.config.set('outlander-ui.showBlur', false)
+
+    const workspaceElement = atom.views.getView(atom.workspace)
+    expect(workspaceElement.classList.contains(CLASSNAME)).toBe(false)
+
+    const testModal = atom.workspace.addModalPanel({item: document.createElement('div')})
+
     expect(workspaceElement.classList.contains(CLASSNAME)).toBe(false)
   })
 })
